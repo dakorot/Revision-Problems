6e28642031c3154b8bd4157378a4d4643c865062
dakorot <koropenko.daria@gmail.com>
2024-12-03 21:17:56 +0100

    Gaussian noise implemented using Marsaglia Polar method + comments

--- gaussian_noise.cpp
+++ gaussian_noise.cpp
@@ -21,11 +21,14 @@ float marsaglia_gaussian_noise(float sigma, float mean)
         s = pow(u, 2) + pow(v, 2);
     } while (s>=1.0 || s==0.0);
 
+    //remembering the second unused random value to use next time
     random_value_in_queue = v * s;
     value_in_queue = true;
 
+    //from Box-Muller transform
     s = sqrt((-2.0)*log(s)/s);
 
+    //from Marsaglia Polar method
     return mean + sigma * u * s;
 }
 
