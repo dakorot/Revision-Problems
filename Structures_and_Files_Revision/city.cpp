73384abc2fae1777c8cef173f29b1715ea7a6cd9
dakorot <koropenko.daria@gmail.com>
2025-02-07 08:22:43 +0100

    City structure from File Revision (DONE)

--- Structures_and_Files_Revision/city.cpp
+++ Structures_and_Files_Revision/city.cpp
@@ -30,25 +30,23 @@ void print_cities_info(City* src, int size)
 
 int main()
 {
-    unsigned int size = 3, cols = 4, row = 0;
+    unsigned int size = 3, row = 0;
     FILE* cities_file = fopen("/home/daria/CLionProjects/Revision-Problems/Structures_and_Files_Revision/cities.csv", "r");
     City* cities_list = new City[size];
     char buffer[256] = {};
     if(cities_file)
     {
-        while(fgets(buffer, sizeof(buffer), cities_file) && (row-2 < size))
+        while(fgets(buffer, sizeof(buffer), cities_file))
         {
-//            printf("buffer: %s\n", buffer);
             ++row;
             if(row == 1) continue;
 
-            char *name = new char[51];
+            char name[51] = {};
             unsigned int citizens = 0;
             double x = 0.0, y = 0.0;
 
             char* token = std::strtok(buffer, ",");
-            while(token)
-            {
+
                 std::strcpy(name, token);
                 unsigned int name_len = std::strlen(token);
                 name[name_len+1] = 0;
@@ -63,8 +61,6 @@ int main()
                 y = std::strtod(token, nullptr);
 
                 cities_list[row-2] = init_city(name, citizens, x, y);
-            }
-        delete[] name;
         }
     }
     print_cities_info(cities_list, size);
